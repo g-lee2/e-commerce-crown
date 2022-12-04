f8cbaa5aebe8a554c1de35e3cf57547f7569264e
Grace Lee <gracexsnoopy@gmail.com>
2022-12-03 22:19:03 -0800

    Create a directory component and update directory with category item component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import './categories.styles.scss';
 import './components/category-item/category-item.component';
-import CategoryItem from './components/category-item/category-item.component';
+import Directory from './components/directory/directory.component';
 
 const App = () => {
   const categories = [
@@ -32,11 +31,7 @@ const App = () => {
   ];
 
   return (
-    <div className="categories-container">
-      {categories.map((category ) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
+    <Directory categories={categories} />
   );
 };
 
