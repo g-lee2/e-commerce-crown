1f62ac0d6db80ba433a2ff9359ad903b4ad25a08
Grace Lee <gracexsnoopy@gmail.com>
2022-12-13 00:34:50 -0800

    Add styling and add a link that renders the shop component

--- src/routes/navigation/navigation.component.js
+++ src/routes/navigation/navigation.component.js
@@ -1,22 +1,24 @@
-import { Outlet } from 'react-router-dom';
-import { Fragment, Link } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import { Fragment } from 'react';
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import './navigation.styles.scss';
 
 const Navigation = () => {
   return (
     <Fragment>
       <div className='navigation'>
         <Link className='logo-container' to='/'>
-          <div>logo</div>
+          <CrwnLogo className='logo' />
         </Link>
-        {/* <div className='nav-links-container'> 
+        <div className='nav-links-container'>
           <Link className='nav-link' to='/shop'>
             SHOP
           </Link>
-        </div> */}
+        </div>
       </div>
       <Outlet />
     </Fragment>
-  )
-}
+  );
+};
 
 export default Navigation;
