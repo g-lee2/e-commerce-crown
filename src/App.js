f492b0ea4c36e1ba83980833f059707ce46dee0f
Grace Lee <gracexsnoopy@gmail.com>
2022-12-12 19:23:38 -0800

    Add a route that renders home component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import Home from './routes/home/home.component'
+import Home from './routes/home/home.component';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <Home />
+    <Routes>
+      <Route path='/' element={ <Home /> }/>
+    </Routes>
   );
 };
 
